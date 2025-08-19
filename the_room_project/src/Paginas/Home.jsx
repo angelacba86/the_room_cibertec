12650be411a97a63a2be89e37b38080ae672a5db
Miguel <miguel.071193@gmail.com>
2025-08-18 22:21:45 -0500

    tarjetashome

--- the_room_project/src/Paginas/Home.jsx
+++ the_room_project/src/Paginas/Home.jsx
@@ -4,115 +4,13 @@ import "./Home.css"
 import PreguntasHome from "../Modulos/PreguntasHome"
 import PlanesHome from "../Modulos/PlanesHome"
 import BannerTrial from "../Modulos/BannerTrial"
+import TarjetasHome from "../Modulos/TarjetasHome"
 function Home(){
     return(
         <>
         <Banner></Banner>
         <main>
-            <div className="contenedor1">
-                <div className="parte1">
-                    <h3>
-                        We Provide you streaming experience across various devices.
-                    </h3>
-                    <p>
-                    With StreamVibe, you can enjoy your favorite movies and TV shows anytime, anywhere. Our platform is designed to be compatible with a wide range of devices, ensuring that you never miss a moment of entertainment.
-                    </p>    
-                </div>
-
-                <div className="contenedorTarjetas">
-                    <div className="tarjeta1">
-                        <div className="seccion1">
-                            <img src="./icono1.png" alt="Smartphones" />
-                            <h4>
-                                Smartphones
-                            </h4>
-                        </div>
-                        <div className="seccion2">
-                            <p>
-                                StreamVibe is optimized for both Android and iOS smartphones. Download our app from the Google Play Store or the Apple App Store
-                            </p>
-                        </div>
-                    </div>
-
-                    <div className="tarjeta1">
-                        <div className="seccion1">
-                            <img src="./icono2.png" alt="Tablet" />
-                            <h4>
-                                Tablet
-                            </h4>
-                        </div>
-                        <div className="seccion2">
-                            <p>
-                                StreamVibe is optimized for both Android and iOS smartphones. Download our app from the Google Play Store or the Apple App Store
-                            </p>
-                        </div>
-                    </div>
-
-                    <div className="tarjeta1">
-                        <div className="seccion1">
-                            <img src="./icono3.png" alt="Smart TV" />
-                            <h4>
-                                Smart TV
-                            </h4>
-                        </div>
-                        <div className="seccion2">
-                            <p>
-                                StreamVibe is optimized for both Android and iOS smartphones. Download our app from the Google Play Store or the Apple App Store
-                            </p>
-                        </div>
-                    </div>
-
-
-
-
-                    <div className="tarjeta1">
-                        <div className="seccion1">
-                            <img src="./icono4.png" alt="Laptops" />
-                            <h4>
-                                Laptops
-                            </h4>
-                        </div>
-                        <div className="seccion2">
-                            <p>
-                                StreamVibe is optimized for both Android and iOS smartphones. Download our app from the Google Play Store or the Apple App Store
-                            </p>
-                        </div>
-                    </div>
-
-                    <div className="tarjeta1">
-                        <div className="seccion1">
-                            <img src="./icono5.png" alt="Gaming Consoles" />
-                            <h4>
-                                Gaming Consoles
-                            </h4>
-                        </div>
-                        <div className="seccion2">
-                            <p>
-                                StreamVibe is optimized for both Android and iOS smartphones. Download our app from the Google Play Store or the Apple App Store
-                            </p>
-                        </div>
-                    </div>
-
-                    <div className="tarjeta1">
-                        <div className="seccion1">
-                            <img src="./icono6.png" alt="VR Headsets" />
-                            <h4>
-                                VR Headsets 
-                            </h4>
-                        </div>
-                        <div className="seccion2">
-                            <p>
-                                StreamVibe is optimized for both Android and iOS smartphones. Download our app from the Google Play Store or the Apple App Store
-                            </p>
-                        </div>
-                    </div>
-
-                
-
-
-
-                </div>
-            </div>
+        <TarjetasHome></TarjetasHome>
         <PreguntasHome></PreguntasHome>
         <PlanesHome></PlanesHome>
         <BannerTrial></BannerTrial>
