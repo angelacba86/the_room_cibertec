abb8a1e2d9dc1eba991c4f8bbc2fb605735a484b
Richard <rigetobu1997@egmail.com>
2025-08-18 23:36:18 -0500

    Traduccion Footer/Planes

--- the_room_project/src/Modulos/Footer.jsx
+++ the_room_project/src/Modulos/Footer.jsx
@@ -7,15 +7,15 @@ function Footer() {
                 <nav className="f-top">
                     <div className="menu-link">
                         <ul className="link">
-                            <li><a href="#">Home</a></li>
-                            <li><a href="#">Movies</a></li>
-                            <li><a href="#">Shows</a></li>
+                            <li><a href="#">Inicio</a></li>
+                            <li><a href="#">Peliculas</a></li>
+                            <li><a href="#">Series</a></li>
                             <li><a href="#">Suscripcion</a></li>
                         </ul>
                     </div>
 
                     <div className="social-wrap">
-                        <h3>Connect With Us</h3>
+                        <h3>Contactanos</h3>
                         <div className="social">
                             <a href="https://facebook.com"><img src="/Facebook.png" alt="Facebook" /></a>
                             <a href="https://twitter.com"><img src="/Twitter.png" alt="Twitter" /></a>
@@ -27,9 +27,9 @@ function Footer() {
                 <div className="f-bottom">
                     <h3>© 2025 streamvib, All Rights Reserved</h3>
                     <div className="politicas">
-                        <a href="#">Terms of Use</a>
-                        <a href="#">Privacy Policy</a>
-                        <a href="#">Cookkie Policy</a>
+                        <a href="#">Terminos de uso</a>
+                        <a href="#">Politicas de Privacidad</a>
+                        <a href="#">Politicas de Cookkie</a>
                     </div>
                 </div>
             </div>
