16d0323101c393b911d033f75c295ed1ab20147c
Jhurice <juan.1991.14.fe@gmail.com>
2025-08-19 01:45:33 -0500

    BannerTrial

--- the_room_project/src/Modulos/BannerTrial.jsx
+++ the_room_project/src/Modulos/BannerTrial.jsx
@@ -1,8 +1,19 @@
-function BannerTrial() {
+import "./BannerTrial.css";
+export default function BannerTrial() {
   return (
-    <>
-      <h2>Aca va el Banner Trial</h2>
-    </>
-  );
+    <div className="contenedor">
+      <div className="img-fondo">
+        <div>
+          <div className="texto-banner">
+          <h2>Start your free trial today!</h2>
+          <p>
+            This is a clear and concise call to action that encourages users to
+            sign up for a free trial of StreamVibe.
+          </p>
+          </div>
+          <button className="boton-trial">Start Free Trial</button>
+        </div>
+      </div>
+    </div>
+  );
 }
-export default BannerTrial;
