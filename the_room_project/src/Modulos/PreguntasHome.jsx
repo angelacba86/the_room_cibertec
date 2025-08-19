893ccda95584522eebf15616f97521a7a956e985
Angela Barrenechea <angelacba86@gmail.com>
2025-08-18 22:24:15 -0500

    preguntasHome

--- the_room_project/src/Modulos/PreguntasHome.jsx
+++ the_room_project/src/Modulos/PreguntasHome.jsx
@@ -1,125 +1,103 @@
 import "./PreguntasHome.css";
+import { useState } from "react";
 function PreguntasHome() {
+  const preguntas = [
+    {
+      nro: "01",
+      titulo: "¿Que es StreamVibe?",
+      texto:
+        "StreamVibe es una plataforma de streaming que te permite ver películas, series y documentales en cualquier momento y lugar.",
+    },
+    {
+      nro: "02",
+      titulo: "¿Cuanto cuesta StreamVibe ?",
+      texto:
+        "StreamVibe ofrece planes de suscripción mensuales a precios accesibles. El costo puede variar según el plan que elijas.",
+    },
+    {
+      nro: "03",
+      titulo: "¿Que contenido esta disponible en StreamVibe?",
+      texto:
+        "En StreamVibe encontrarás una amplia variedad de películas, series, documentales y producciones exclusivas, tanto nacionales como internacionales.",
+    },
+    {
+      nro: "04",
+      titulo: "¿Cómo puedo ver StreamVibe?",
+      texto:
+        "Puedes acceder a StreamVibe desde cualquier dispositivo con conexión a internet, ya sea computadora, tablet, smartphone o smart TV.",
+    },
+    {
+      nro: "05",
+      titulo: "¿Cómo me inscribo a StreamVibe?",
+      texto:
+        "Solo necesitas crear una cuenta en nuestra página web o aplicación, elegir un plan de suscripción y completar el registro.",
+    },
+    {
+      nro: "06",
+      titulo: "¿Cómo funciona el servicio de prueba gratuito de StreamVibe?",
+      texto:
+        "StreamVibe ofrece un periodo de prueba gratuito de 07 días durante el cual puedes acceder a todo el catálogo sin costo. Al finalizar, podrás decidir si deseas continuar con una suscripción de pago.",
+    },
+    {
+      nro: "07",
+      titulo:
+        "¿Como me contacto con el servicio de soporte al cliente de StreamVibe?",
+      texto:
+        "Puedes comunicarte con nuestro equipo de soporte a través del chat en línea, correo electrónico o la sección de ayuda en la aplicación.",
+    },
+    {
+      nro: "08",
+      titulo: "¿Cuales son los métodos de pago de StreamVibe?",
+      texto:
+        "Aceptamos diversos métodos de pago, incluyendo tarjetas de crédito y débito, billeteras digitales y pagos en línea según tu país.",
+    },
+  ];
+  const [preguntaAbierta, setPreguntaAbierta] = useState(null);
+
+  const switchPregunta = (index) => {
+    setPreguntaAbierta(preguntaAbierta === index ? null : index);
+  };
+
   return (
     <>
       <div className="contenedor">
         <h2>Frequently Asked Questions</h2>
-        <p>
+        <p className="parrafo-principal">
           Got questions? We've got answers! Check out our FAQ section to find
           answers to the most common questions about StreamVibe.
         </p>
+
         <div className="preguntas-contenedor">
-          <div className="pregunta">
-            <h3>01</h3>
-            <div>
-              <h4>What is StreamVibe?</h4>
-              <p>
-                StreamVibe is a streaming service that allows you to watch
-                movies and shows on demand.
-              </p>
-            </div>
+          {preguntas.map((pregunta, index) => (
+            <div className="pregunta" key={index}>
+              <div>
+                <h3 className="nro-pregunta" >
+                  {pregunta.nro}
+                </h3>
+                <div className="textos-pregunta">
+                  <h4>{pregunta.titulo}</h4>
+                  <div
+                    className={`respuesta ${
+                      preguntaAbierta === index ? "abierto" : ""
+                    }`}
+                  >
+                    <p>{pregunta.texto}</p>
+                  </div>
+                </div>
 
-            <div>
-              <h2>+</h2>
+                <a
+                  href="#"
+                  onClick={(e) => {
+                    e.preventDefault();
+                    switchPregunta(index);
+                  }}
+                >
+                  {preguntaAbierta === index ? "−" : "+"}
+                </a>
+              </div>
+              <div className="linea-degradado"></div>
             </div>
-          </div>
-          <div className="pregunta">
-            <h3>02</h3>
-            <div>
-              <h4>How much does StreamVibe cost?</h4>
-              <p>
-                StreamVibe is a streaming service that allows you to watch
-                movies and shows on demand.
-              </p>
-            </div>
-
-            <div>
-              <h2>+</h2>
-            </div>
-          </div>
-          <div className="pregunta">
-            <h3>03</h3>
-            <div>
-              <h4>What content is available on StreamVibe?</h4>
-              <p>
-                StreamVibe is a streaming service that allows you to watch
-                movies and shows on demand.
-              </p>
-            </div>
-            <div>
-              <h2>+</h2>
-            </div>
-          </div>
-          <div className="pregunta">
-            <h3>04</h3>
-            <div>
-              <h4>How can I watch StreamVibe?</h4>
-              <p>
-                StreamVibe is a streaming service that allows you to watch
-                movies and shows on demand.
-              </p>
-            </div>
-
-            <div>
-              <h2>+</h2>
-            </div>
-          </div>
-          <div className="pregunta">
-            <h3>05</h3>
-            <div>
-              <h4>How do I sign up for StreamVibe?</h4>
-              <p>
-                StreamVibe is a streaming service that allows you to watch
-                movies and shows on demand.
-              </p>
-            </div>
-
-            <div>
-              <h2>+</h2>
-            </div>
-          </div>
-          <div className="pregunta">
-            <h3>06</h3>
-            <div>
-              <h4>What is StreamVibe?</h4>
-              <p>
-                StreamVibe is a streaming service that allows you to watch
-                movies and shows on demand.
-              </p>
-            </div>
-
-            <div>
-              <h2>+</h2>
-            </div>
-          </div>
-          <div className="pregunta">
-            <h3>07</h3>
-            <div>
-              <h4>What is StreamVibe?</h4>
-              <p>
-                StreamVibe is a streaming service that allows you to watch
-                movies and shows on demand.
-              </p>
-            </div>
-
-            <div>
-              <h2>+</h2>
-            </div>
-          </div>
-          <div className="pregunta">
-            <h3>01</h3>
-            <div>
-              <h4>What is StreamVibe?</h4>
-              <p>
-                StreamVibe is a streaming service that allows you to watch
-                movies and shows on demand.
-              </p>
-            </div>
-
-            <div>
-              <h2>+</h2>
-            </div>
-          </div>
+          ))}
         </div>
       </div>
     </>
