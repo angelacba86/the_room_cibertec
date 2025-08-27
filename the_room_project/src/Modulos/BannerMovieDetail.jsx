b38d1394fdad234b0743b2eda44fd549b6c497aa
Angela Barrenechea <angelacba86@gmail.com>
2025-08-26 20:07:43 -0500

    cambiar color de degradado

--- the_room_project/src/Modulos/BannerMovieDetail.jsx
+++ the_room_project/src/Modulos/BannerMovieDetail.jsx
@@ -1,17 +1,20 @@
-import "./BannerMoviesDetail.css"
+import "./BannerMoviesDetail.css";
 function BannerMovieDetail({ movie }) {
   if (!movie) return null;
-  console.log(movie.banner)
+  console.log(movie.banner);
   return (
-    <div className="Contenedor-BannerMovies" style={{
+    <div
+      className="Contenedor-BannerMovies"
+      style={{
         backgroundImage: `linear-gradient(
-          to top,
-          rgba(0, 0, 0, 1) 0%,
-          rgba(0, 0, 0, 0.85) 35%,
-          rgba(0, 0, 0, 0.6) 60%,
-          rgba(0, 0, 0, 0) 100%
+      to top,
+      rgba(20, 20, 20, 1) 0%,
+      rgba(0, 0, 0, 0.85) 35%,
+      rgba(0, 0, 0, 0.6) 60%,
+      rgba(0, 0, 0, 0) 100%
         ), url(${movie.banner})`,
-      }} >
+      }}
+    >
       <div className="TextoBannerMovies">
         <h1>{movie.title}</h1>
         <p>{movie.description}</p>
