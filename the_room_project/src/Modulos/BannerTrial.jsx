2358a3c4c989c13e726cbf94ba6742576cf94617
Angela Barrenechea <angelacba86@gmail.com>
2025-08-26 20:08:50 -0500

    forzar estilos trial sobre contenedor

--- the_room_project/src/Modulos/BannerTrial.jsx
+++ the_room_project/src/Modulos/BannerTrial.jsx
@@ -1,18 +1,21 @@
 import "./BannerTrial.css";
 export default function BannerTrial() {
   return (
-    <div className="contenedor">
+    <div className="contenedor trial">
       <div className="img-fondo">
         <div>
           <div className="texto-banner">
-          <h2>¡Empieza tu prueba gratuita hoy!</h2>
-          <p>
-            Se trata de una llamada a la acción clara y concisa que anima a los usuarios a registrarse para obtener una prueba gratuita de StreamVibe.
-          </p>
+            <h2>¡Empieza tu prueba gratuita hoy!</h2>
+            <p>
+              Se trata de una llamada a la acción clara y concisa que anima a
+              los usuarios a registrarse para obtener una prueba gratuita de
+              The Room.
+            </p>
           </div>
-          <button className="boton-trial">Start Free Trial</button>
+          <button className="boton-trial">Prueba Gratis</button>
         </div>
       </div>
-    </div>
-  );
+         
+    </div>
+  );
 }
